3722eed9699d4949c1b0f20e7c980c0e1d076885
18770083353 <252625092@qq.com>
2020-06-30 21:17:47 +0800

    提交说明

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App.vue'
+// import { Button, Field } from 'vant'
+// import 'amfe-flexible'
+// import Vant from 'vant'
+// import 'vant/lib/index.css'
+
+// 把vant中所有的组件都导入了
+// Vue.use(Vant)
+// Vue.use(Button)
+// Vue.use(Field)
+
+Vue.config.productionTip = false
+
+new Vue({
+    render: h => h(App)
+}).$mount('#app')
